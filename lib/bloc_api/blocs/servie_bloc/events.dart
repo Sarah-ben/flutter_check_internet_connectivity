43f6defa3bc1db8c4e1ee3a1a3e3bcd9e3f0f992
Sarah-ben <70490047+Sarah-ben@users.noreply.github.com>
2022-10-22 20:56:59 +0200

    Delete lib/bloc_api/blocs/servie_bloc directory

--- lib/bloc_api/blocs/servie_bloc/events.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class ServiceEvent extends Equatable{
-}
-
-class LoadServiceEvent extends ServiceEvent{
-  @override
-  List<Object?> get props => [];
-}
